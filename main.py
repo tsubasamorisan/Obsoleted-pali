9129006868914bf88f91af2a77bad3d8325caa17
tsubasamorisan <tsubasamori.eros@gmail.com>
2012-06-05 03:27:37 +0800

    use jinja2 templating to simplify code

--- main.py
+++ main.py
@@ -10,73 +10,11 @@ from dictionary import lookup
 jinja_environment = jinja2.Environment(
   loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
-google_analytic_code = u"""
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-32179549-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-"""
-
-google_translate_code = u"""
-<div id="google_translate_element"></div><script>
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({
-    pageLanguage: 'en',
-    multilanguagePage: true,
-    gaTrack: true,
-    gaId: 'UA-32179549-1',
-    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-  }, 'google_translate_element');
-}
-</script><script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-"""
-
-google_upper_ad_code = u"""
-<script type="text/javascript"><!--
-google_ad_client = "ca-pub-0436733829999264";
-/* 上方橫幅 */
-google_ad_slot = "1134049895";
-google_ad_width = 320;
-google_ad_height = 50;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-"""
-
-google_right_ad_code = u"""
-<script type="text/javascript"><!--
-google_ad_client = "ca-pub-0436733829999264";
-/* 右方直條 */
-google_ad_slot = "8422986659";
-google_ad_width = 160;
-google_ad_height = 600;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-"""
-
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
     result = ""
     template_values = {
-      'google_analytic'  : google_analytic_code,
-      'google_translate' : google_translate_code,
-      'right_ads'        : google_right_ad_code,
-      'upper_ads'        : google_upper_ad_code,
       'result'           : result,
     }
 
@@ -90,10 +28,6 @@ class AboutPage(webapp2.RequestHandler):
 <p style="padding-left:30px; padding-right:30px">The dictionary come from <a href="http://online-dhamma.net/anicca/pali-course/Pali-Chinese-English%20Dictionary.html">Pali-Chinese-English Dictionary</a>. The version is "Pali Dict Linux Web Ver 1.0". This site is still under development so errors may be encountered. If any questions or suggestions, please <a href="/contact">contact me</a>.</p>
               """
     template_values = {
-      'google_analytic'  : google_analytic_code,
-      'google_translate' : google_translate_code,
-      'right_ads'        : google_right_ad_code,
-      'upper_ads'        : google_upper_ad_code,
       'result'           : result,
     }
 
@@ -115,10 +49,6 @@ Other links about Buddhism:<br />
 </div>
               """
     template_values = {
-      'google_analytic'  : google_analytic_code,
-      'google_translate' : google_translate_code,
-      'right_ads'        : google_right_ad_code,
-      'upper_ads'        : google_upper_ad_code,
       'result'           : result,
     }
 
@@ -130,10 +60,6 @@ class ContactPage(webapp2.RequestHandler):
   def get(self):
     result = u"""<br /><br />My Email : <img src="/static/mail.png" />"""
     template_values = {
-      'google_analytic'  : google_analytic_code,
-      'google_translate' : google_translate_code,
-      'right_ads'        : google_right_ad_code,
-      'upper_ads'        : google_upper_ad_code,
       'result'           : result,
     }
 
@@ -149,10 +75,6 @@ class Lookup(webapp2.RequestHandler):
     word = cgi.escape(self.request.get('word'))
     result = lookup(word)
     template_values = {
-      'google_analytic'  : google_analytic_code,
-      'google_translate' : google_translate_code,
-      'right_ads'        : google_right_ad_code,
-      'upper_ads'        : google_upper_ad_code,
       'result'           : result,
     }
 
