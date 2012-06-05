de63dd1976f9ff77d78e462f07fc72f7cbf17fc8
tsubasamorisan <tsubasamori.eros@gmail.com>
2012-06-06 05:10:36 +0800

    use javascript to show adout, link, and contact page

--- main.py
+++ main.py
@@ -22,51 +22,6 @@ class MainPage(webapp2.RequestHandler):
     self.response.out.write(template.render(template_values))
 
 
-class AboutPage(webapp2.RequestHandler):
-  def get(self):
-    result = u"""<br /><br />
-<p style="padding-left:30px; padding-right:30px">The dictionary come from <a href="http://online-dhamma.net/anicca/pali-course/Pali-Chinese-English%20Dictionary.html">Pali-Chinese-English Dictionary</a>. The version is "Pali Dict Linux Web Ver 1.0". This site is still under development so errors may be encountered. If any questions or suggestions, please <a href="/contact">contact me</a>.</p>
-              """
-    template_values = {
-      'result'           : result,
-    }
-
-    template = jinja_environment.get_template('index.html')
-    self.response.out.write(template.render(template_values))
-
-
-class LinkPage(webapp2.RequestHandler):
-  def get(self):
-    result = u"""<br /><br />
-<div align="left" style="padding-left:30px; padding-right:30px">
-Source Code : <a href="http://code.google.com/p/pali/">pali</a> project on Google Code<br />
-Dictionary : from "Pali Dict Linux Web Ver 1.0" on <a href="http://online-dhamma.net/anicca/pali-course/Pali-Chinese-English%20Dictionary.html">Pali-Chinese-English Dictionary</a><br />
-<br />
-Other links about Buddhism:<br />
-<a href="http://www.theravadacn.org/DhammaIndex2.htm">覺醒之翼</a><br />
-<a href="http://www.wpp-branches.net/cn/index.php">巴蓬寺分院</a><br />
-<a href="http://www.dhamma.net.cn/">覺悟之路</a><br />
-</div>
-              """
-    template_values = {
-      'result'           : result,
-    }
-
-    template = jinja_environment.get_template('index.html')
-    self.response.out.write(template.render(template_values))
-
-
-class ContactPage(webapp2.RequestHandler):
-  def get(self):
-    result = u"""<br /><br />My Email : <img src="/static/mail.png" />"""
-    template_values = {
-      'result'           : result,
-    }
-
-    template = jinja_environment.get_template('index.html')
-    self.response.out.write(template.render(template_values))
-
-
 class Lookup(webapp2.RequestHandler):
   def get(self):
     self.redirect('/')
@@ -83,8 +38,5 @@ class Lookup(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                              ('/about', AboutPage),
-                              ('/link', LinkPage),
-                              ('/contact', ContactPage),
                               ('/lookup', Lookup)],
                               debug=True)
--- static/palidict.js
+++ static/palidict.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 });
 
 
-<!-- make keypad show if hidden, hide if shown -->
+//<!-- make keypad show if hidden, hide if shown -->
 function toggle() {
   var kb = document.getElementById("keyboard");
   var dt = document.getElementById("displayText");
@@ -22,4 +22,23 @@ function toggle() {
     kb.style.display = "inline";
     dt.innerHTML = "Hide Pāli Keypad";
   }
-} 
+}
+//<!-- http://www.web-code.org/coding-tools/javascript-escape-unescape-converter-tool.html -->
+function showAbout() {
+  var rt = document.getElementById("result");
+  rt.innerHTML = unescape(
+"%3Cbr%20/%3E%3Cbr%20/%3E%3Cp%20style%3D%22padding-left%3A30px%3B%20padding-right%3A30px%22%3EThe%20dictionary%20come%20from%20%3Ca%20href%3D%22http%3A//online-dhamma.net/anicca/pali-course/Pali-Chinese-English%2520Dictionary.html%22%3EPali-Chinese-English%20Dictionary%3C/a%3E.%20The%20version%20is%20%22Pali%20Dict%20Linux%20Web%20Ver%201.0%22.%20This%20site%20is%20still%20under%20development%20so%20errors%20may%20be%20encountered.%20If%20any%20questions%20or%20suggestions%2C%20please%20%3Ca%20href%3D%22/contact%22%3Econtact%20me%3C/a%3E.%3C/p%3E"
+);
+}
+function showLink() {
+  var rt = document.getElementById("result");
+  rt.innerHTML = unescape(
+"%3Cbr%20/%3E%3Cbr%20/%3E%0A%3Cdiv%20align%3D%22left%22%20style%3D%22padding-left%3A30px%3B%20padding-right%3A30px%22%3E%0ASource%20Code%20%3A%20%3Ca%20href%3D%22http%3A//code.google.com/p/pali/%22%3Epali%3C/a%3E%20project%20on%20Google%20Code%3Cbr%20/%3E%0ADictionary%20%3A%20from%20%22Pali%20Dict%20Linux%20Web%20Ver%201.0%22%20on%20%3Ca%20href%3D%22http%3A//online-dhamma.net/anicca/pali-course/Pali-Chinese-English%2520Dictionary.html%22%3EPali-Chinese-English%20Dictionary%3C/a%3E%3Cbr%20/%3E%0A%3Cbr%20/%3E%0AOther%20links%20about%20Buddhism%3A%3Cbr%20/%3E%0A%3Ca%20href%3D%22http%3A//www.theravadacn.org/DhammaIndex2.htm%22%3E%u89BA%u9192%u4E4B%u7FFC%3C/a%3E%3Cbr%20/%3E%0A%3Ca%20href%3D%22http%3A//www.wpp-branches.net/cn/index.php%22%3E%u5DF4%u84EC%u5BFA%u5206%u9662%3C/a%3E%3Cbr%20/%3E%0A%3Ca%20href%3D%22http%3A//www.dhamma.net.cn/%22%3E%u89BA%u609F%u4E4B%u8DEF%3C/a%3E%3Cbr%20/%3E%0A%3C/div%3E%0A"
+);
+}
+function showContact() {
+  var rt = document.getElementById("result");
+  rt.innerHTML = unescape(
+"%3Cbr%20/%3E%3Cbr%20/%3EMy%20Email%20%3A%20%3Cimg%20src%3D%22/static/mail.png%22%20/%3E"
+);
+}
