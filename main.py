198eff2a77293b6c87633ccebf0fbe773b7d4980
tsubasamorisan <tsubasamori.eros@gmail.com>
2012-06-19 05:40:55 +0800

    serve page according to locale and accept-languages

--- main.py
+++ main.py
@@ -20,11 +20,50 @@ jinja_environment.install_gettext_translations(i18n)
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
-    #lang = self.request.headers.get('accept_language')
+    #locale = self.request.GET.get('locale', 'en_US')
+    locale = self.request.GET.get('locale')
+    if (locale):
+      if locale not in ['en_US', 'zh_TW', 'zh_CN']:
+        locale = 'en_US'
+        i18n.get_i18n().set_locale(locale)
+        #self.response.out.write("locale: en_US")
+      else:
+        i18n.get_i18n().set_locale(locale)
+        #self.response.out.write("locale: %s" % locale)
+    else:
+      #self.response.out.write("no locale")
+      accept_languages = self.request.headers.get('accept_language')
+      languages = accept_languages.split(",")
+      language_q_pairs = []
+      for language in languages:
+        if language.split(";")[0] == language:
+          language_q_pairs.append((language, "1"))
+        else:
+          locale = language.split(";")[0]
+          q = language.split(";")[1].split("=")[1]
+          language_q_pairs.append((locale, q))
+      #self.response.out.write(language_q_pairs)
+      if (language_q_pairs[0][0].lower() == 'zh-tw'):
+        locale = 'zh_TW'
+        i18n.get_i18n().set_locale(locale)
+        #self.response.out.write("locale: zh_TW")
+      elif (language_q_pairs[0][0].lower() == 'zh-hk'):
+        locale = 'zh_TW'
+        i18n.get_i18n().set_locale(locale)
+        #self.response.out.write("locale: zh_TW")
+      elif (language_q_pairs[0][0].lower() == 'zh-cn'):
+        locale = 'zh_CN'
+        i18n.get_i18n().set_locale(locale)
+        #self.response.out.write("locale: zh_CN")
+      elif (language_q_pairs[0][0][:2].lower() == 'zh'):
+        locale = 'zh_CN'
+        i18n.get_i18n().set_locale(locale)
+        #self.response.out.write("locale: zh_CN")
+      else:
+        locale = 'en_US'
+        i18n.get_i18n().set_locale(locale)
+        #self.response.out.write("locale: en_US")
     #browser = self.request.headers.get('user_agent')
-
-    locale = self.request.GET.get('locale', 'en_US')
-    i18n.get_i18n().set_locale(locale)
 
     """
     # https://developers.google.com/appengine/docs/python/runtime#The_Environment
@@ -46,7 +85,7 @@ class MainPage(webapp2.RequestHandler):
       'zh_TW' : u'中文(繁體)',
     }
     href = {
-      'en_US' : u'/',
+      'en_US' : u'/?locale=en_US',
       'zh_CN' : u'/?locale=zh_CN',
       'zh_TW' : u'/?locale=zh_TW',
     }
