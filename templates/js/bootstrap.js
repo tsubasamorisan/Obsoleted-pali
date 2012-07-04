db2a638e5d70473a58137d18622ee513892edeed
tsubasamorisan <tsubasamori.eros@gmail.com>
2012-07-05 03:46:23 +0800

    re-write bootstrap.js

--- templates/js/bootstrap.js
+++ templates/js/bootstrap.js
@@ -1,41 +1,17 @@
 /* Reference: http://ntt.cc/2008/02/10/4-ways-to-dynamically-load-external-javascriptwith-source.html */
-function AJAXLoad(type, loadByXMLHttp, url){
-  var ext;
-  if (type == "js") {
-    ext = document.createElement('script');
-    ext.setAttribute("type","text/javascript");
-  }
-  if (type == "css") {
-    ext = document.createElement('link');
-    ext.setAttribute("type", "text/css");
-    ext.setAttribute("rel", "stylesheet");
-  }
-  if (!loadByXMLHttp) {
-    if (type == "js") {
-      ext.setAttribute("src", url);
-      if (typeof ext != "undefined") {document.getElementsByTagName("head")[0].appendChild(ext);}
-    }
-    if (type == "css") {
-      ext.setAttribute("href", url);
-      if (typeof ext != "undefined") {document.getElementsByTagName("head")[0].appendChild(ext);}
-    }
-    return;
-  }
-  var xmlhttp;
-  if (window.XMLHttpRequest) {xmlhttp=new XMLHttpRequest();}
-  else {xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");}
-  xmlhttp.onreadystatechange=function(){
-    if (xmlhttp.readyState==4) {
-      if (xmlhttp.status == 200) {
-        ext.innerHTML=xmlhttp.responseText;
-        document.getElementsByTagName("head")[0].appendChild(ext);
-      } else {
-        console.log('cannot load external file :'+url);
-      }
-    }
-  }
-  xmlhttp.open("GET",url,true);
-  xmlhttp.send();
+function LoadJS(url) {
+  var ext = document.createElement('script');
+  ext.setAttribute("type","text/javascript");
+  ext.setAttribute("src", url);
+  if (typeof ext != "undefined") {document.getElementsByTagName("head")[0].appendChild(ext);}
+}
+
+function LoadCSS(url) {
+  var ext = document.createElement('link');
+  ext.setAttribute("type", "text/css");
+  ext.setAttribute("rel", "stylesheet");
+  ext.setAttribute("href", url);
+  if (typeof ext != "undefined") {document.getElementsByTagName("head")[0].appendChild(ext);}
 }
 
 function LoadFavicon(url) {
@@ -72,35 +48,32 @@ var queryURL = function() {
 } ();
 
 /* Load Google Web Fonts */
-AJAXLoad("css", false, "http://fonts.googleapis.com/css?family=Gentium+Basic|Special+Elite&subset=latin,latin-ext");
-/* Load favicon.ico */
-if (queryURL.ugcfh == "yes") {LoadFavicon("http://pali.googlecode.com/git/static/favicon.ico");}
-else if (window.location.host == 'localhost:8080')             {LoadFavicon("favicon.ico");}
-else if (window.location.host == 'palidictionary.appspot.com') {LoadFavicon("favicon.ico");}
-else {LoadFavicon("statics/favicon.ico");}
-/* Load Index of Pāli Words */
-if (queryURL.ugcfh == "yes") {AJAXLoad("js", false, "http://pali.googlecode.com/git/static/jsvarindex.js");}
-else if (window.location.host == 'localhost:8080')             {AJAXLoad("js", false, "static/jsvarindex.js");}
-else if (window.location.host == 'palidictionary.appspot.com') {AJAXLoad("js", false, "static/jsvarindex.js");}
-else {AJAXLoad("js", false, "statics/jsvarindex.js");}
-/* Load CSS */
-if (queryURL.ugcfh == "yes") {AJAXLoad("css", false, "http://pali.googlecode.com/git/static/css/palidict.css");}
-else if (window.location.host == 'localhost:8080')             {AJAXLoad("css", false, "static/css/palidict.css");}
-else if (window.location.host == 'palidictionary.appspot.com') {AJAXLoad("css", false, "static/css/palidict.css");}
-else {AJAXLoad("css", false, "statics/css/palidict.css");}
-/* Load Input Suggestion Library */
-if (queryURL.ugcfh == "yes") {AJAXLoad("js", false, "http://pali.googlecode.com/git/static/js/inputSuggest.js");}
-else if (window.location.host == 'localhost:8080')             {AJAXLoad("js", false, "static/js/inputSuggest.js");}
-else if (window.location.host == 'palidictionary.appspot.com') {AJAXLoad("js", false, "static/js/inputSuggest.js");}
-else {AJAXLoad("js", false, "statics/js/inputSuggest.js");}
-/* Load Helper Library */
-if (queryURL.ugcfh == "yes") {AJAXLoad("js", false, "http://pali.googlecode.com/git/static/js/palidict.js");}
-else if (window.location.host == 'localhost:8080')             {AJAXLoad("js", false, "static/js/palidict.js");}
-else if (window.location.host == 'palidictionary.appspot.com') {AJAXLoad("js", false, "static/js/palidict.js");}
-else {AJAXLoad("js", false, "statics/js/palidict.js");}
+LoadCSS("http://fonts.googleapis.com/css?family=Gentium+Basic|Special+Elite&subset=latin,latin-ext");
+/* Load JS, CSS and Favicon */
+if (queryURL.ugcfh == "yes") {
+  prefix = 'http://pali.googlecode.com/git/';
+  LoadFavicon(prefix + "static/favicon.ico");
+  LoadJS(prefix + "static/jsvarindex.js");
+  LoadJS(prefix + "static/js/inputSuggest.js");
+  LoadJS(prefix + "static/js/palidict.js");
+  LoadCSS(prefix + "static/css/palidict.css");
+} else if ( (window.location.host == 'localhost:8080') || (window.location.host == 'palidictionary.appspot.com') ) {
+  LoadFavicon("favicon.ico");
+  LoadJS("static/jsvarindex.js");
+  LoadJS("static/js/inputSuggest.js");
+  LoadJS("static/js/palidict.js");
+  LoadCSS("static/css/palidict.css");
+} else {
+  LoadFavicon("statics/favicon.ico");
+  LoadJS("statics/jsvarindex.js");
+  LoadJS("statics/js/inputSuggest.js");
+  LoadJS("statics/js/palidict.js");
+  LoadCSS("statics/css/palidict.css");
+}
 
 if (queryURL.track != "no") {
   if (window.location.host != 'localhost:8080') {
+  /* Load Google Analytics Code */
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-32179549-1']);
 _gaq.push(['_trackPageview']);
