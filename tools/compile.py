be1a288fc8f500c475e614c7148afb296ec559a5
tsubasamorisan <tsubasamori.eros@gmail.com>
2012-07-05 17:04:42 +0800

    re-write script for closure compiler

--- tools/compile.py
+++ tools/compile.py
@@ -5,26 +5,34 @@
 import os, sys
 
 compiler = 'java -jar ../closure-library-read-only/compiler/compiler.jar'
+
+input_bootstrap = '--js templates/js/bootstrap.js'
+output_bootstrap_wso = '--js_output_file templates/js/bootstrap-wso.js'
+output_bootstrap_sp =  '--js_output_file templates/js/bootstrap-sp.js'
+output_bootstrap_adv = '--js_output_file templates/js/bootstrap-adv.js'
+
 input_js1 = '--js static/js/palidict.js'
 input_js2 = '--js static/js/inputSuggest.js'
-input_js3 = '--js templates/js/bootstrap.js'
-output_js1_wso = '--js_output_file static/js/palidict-wso.js'
-output_js2_wso = '--js_output_file static/js/inputSuggest-wso.js'
-output_js3_wso = '--js_output_file templates/js/bootstrap-wso.js'
-output_js1_sp = '--js_output_file static/js/palidict-sp.js'
-output_js2_sp = '--js_output_file static/js/inputSuggest-sp.js'
-output_js3_sp = '--js_output_file templates/js/bootstrap-sp.js'
-output_js1_adv = '--js_output_file static/js/palidict-adv.js'
-output_js2_adv = '--js_output_file static/js/inputSuggest-adv.js'
-output_js3_adv = '--js_output_file templates/js/bootstrap-adv.js'
+output_js_wso = '--js_output_file static/js/pali-wso.js'
+output_js_sp =  '--js_output_file static/js/pali-sp.js'
+output_js_adv = '--js_output_file static/js/pali-adv.js'
+
+wso = '--compilation_level WHITESPACE_ONLY'
+sp =  '--compilation_level SIMPLE_OPTIMIZATIONS'
+adv = '--compilation_level ADVANCED_OPTIMIZATIONS'
+
+def usage():
+  print("Usage:")
+  print("$ python compile.py help")
+  print("$ python compile.py showdir")
+  print("$ python compile.py rm")
+  print("$ python compile.py wso")
+  print("$ python compile.py sp")
+  print("$ python compile.py adv")
 
 if __name__ == '__main__':
   if len(sys.argv) != 2:
-    print("Usage:")
-    print("$ python compile.py help")
-    print("$ python compile.py wso")
-    print("$ python compile.py sp")
-    print("$ python compile.py adv")
+    usage()
     sys.exit(1)
 
   if sys.argv[1] == "help":
@@ -35,38 +43,27 @@ if __name__ == '__main__':
     os.system('ls -al templates/js/')
     sys.exit(0)
   elif sys.argv[1] == "rm":
-    os.system('rm static/js/palidict-*.js')
-    os.system('rm static/js/inputSuggest-*.js')
+    os.system('rm static/js/pali-*.js')
     os.system('rm templates/js/bootstrap-*.js')
     sys.exit(0)
   elif sys.argv[1] == "wso":
-    print('%s --compilation_level WHITESPACE_ONLY %s %s' % (compiler, input_js1, output_js1_wso))
-    os.system('%s --compilation_level WHITESPACE_ONLY %s %s' % (compiler, input_js1, output_js1_wso))
-    print('%s --compilation_level WHITESPACE_ONLY %s %s' % (compiler, input_js2, output_js2_wso))
-    os.system('%s --compilation_level WHITESPACE_ONLY %s %s' % (compiler, input_js2, output_js2_wso))
-    print('%s --compilation_level WHITESPACE_ONLY %s %s' % (compiler, input_js3, output_js3_wso))
-    os.system('%s --compilation_level WHITESPACE_ONLY %s %s' % (compiler, input_js3, output_js3_wso))
+    print(    '%s %s %s %s' % (compiler, wso, input_bootstrap, output_bootstrap_wso))
+    os.system('%s %s %s %s' % (compiler, wso, input_bootstrap, output_bootstrap_wso))
+    print(    '%s %s %s %s %s' % (compiler, wso, input_js1, input_js2, output_js_wso))
+    os.system('%s %s %s %s %s' % (compiler, wso, input_js1, input_js2, output_js_wso))
     sys.exit(0)
   elif sys.argv[1] == "sp":
-    print('%s --compilation_level SIMPLE_OPTIMIZATIONS %s %s' % (compiler, input_js1, output_js1_sp))
-    os.system('%s --compilation_level SIMPLE_OPTIMIZATIONS %s %s' % (compiler, input_js1, output_js1_sp))
-    print('%s --compilation_level SIMPLE_OPTIMIZATIONS %s %s' % (compiler, input_js2, output_js2_sp))
-    os.system('%s --compilation_level SIMPLE_OPTIMIZATIONS %s %s' % (compiler, input_js2, output_js2_sp))
-    print('%s --compilation_level SIMPLE_OPTIMIZATIONS %s %s' % (compiler, input_js3, output_js3_sp))
-    os.system('%s --compilation_level SIMPLE_OPTIMIZATIONS %s %s' % (compiler, input_js3, output_js3_sp))
+    print(    '%s %s %s %s' % (compiler, sp, input_bootstrap, output_bootstrap_sp))
+    os.system('%s %s %s %s' % (compiler, sp, input_bootstrap, output_bootstrap_sp))
+    print(    '%s %s %s %s %s' % (compiler, sp, input_js1, input_js2, output_js_sp))
+    os.system('%s %s %s %s %s' % (compiler, sp, input_js1, input_js2, output_js_sp))
     sys.exit(0)
   elif sys.argv[1] == "adv":
-    print('%s --compilation_level ADVANCED_OPTIMIZATIONS %s %s' % (compiler, input_js1, output_js1_adv))
-    os.system('%s --compilation_level ADVANCED_OPTIMIZATIONS %s %s' % (compiler, input_js1, output_js1_adv))
-    print('%s --compilation_level ADVANCED_OPTIMIZATIONS %s %s' % (compiler, input_js2, output_js2_adv))
-    os.system('%s --compilation_level ADVANCED_OPTIMIZATIONS %s %s' % (compiler, input_js2, output_js2_adv))
-    print('%s --compilation_level ADVANCED_OPTIMIZATIONS %s %s' % (compiler, input_js3, output_js3_adv))
-    os.system('%s --compilation_level ADVANCED_OPTIMIZATIONS %s %s' % (compiler, input_js3, output_js3_adv))
+    print(    '%s %s %s %s' % (compiler, adv, input_bootstrap, output_bootstrap_adv))
+    os.system('%s %s %s %s' % (compiler, adv, input_bootstrap, output_bootstrap_adv))
+    print(    '%s %s %s %s %s' % (compiler, adv, input_js1, input_js2, output_js_adv))
+    os.system('%s %s %s %s %s' % (compiler, adv, input_js1, input_js2, output_js_adv))
     sys.exit(0)
   else:
-    print("Usage:")
-    print("$ python compile.py help")
-    print("$ python compile.py wso")
-    print("$ python compile.py sp")
-    print("$ python compile.py adv")
+    usage()
     sys.exit(1)
