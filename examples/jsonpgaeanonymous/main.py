9ae3194e418da80bfc2f17ac0373e1766b0589e8
tsubasamorisan <tsubasamori.eros@gmail.com>
2012-07-25 07:54:18 +0800

    JSONP with anonymous callback function example

---
+++ examples/jsonpgaeanonymous/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import webapp2
+import urllib2
+import json
+
+
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.out.write(open("index.html").read())
+
+
+class JSONPPage(webapp2.RequestHandler):
+  def get(self):
+    input1 = urllib2.unquote(self.request.get('input1'))
+    input2 = urllib2.unquote(self.request.get('input2'))
+    result = [{ 'input1': input1 },
+              { 'input2': input2 },
+              ('message #1', 'from', 'server'),
+              ('message #2', 'from', 'server')]
+    self.response.headers['Content-Type'] = 'application/javascript'
+    self.response.out.write("(%s)(%s);" %
+                              (urllib2.unquote(self.request.get('callback')),
+                               json.dumps(result)))
+
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/jsonp', JSONPPage)],
+                              debug=True)
