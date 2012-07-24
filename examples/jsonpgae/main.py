b105c3b984f50c934c5892a4306950adcdb6141f
tsubasamorisan <tsubasamori.eros@gmail.com>
2012-07-25 06:48:16 +0800

    use single parentheses instead of double parentheses

--- examples/jsonpgae/main.py
+++ examples/jsonpgae/main.py
@@ -19,7 +19,8 @@ class JSONPPage(webapp2.RequestHandler):
               { 'input2': input2 },
               ('message #1', 'from', 'server'),
               ('message #2', 'from', 'server')]
-    self.response.out.write("%s((%s))" %
+    self.response.headers['Content-Type'] = 'application/javascript'
+    self.response.out.write("%s(%s)" %
                               (urllib2.unquote(self.request.get('callback')),
                                json.dumps(result)))
 
