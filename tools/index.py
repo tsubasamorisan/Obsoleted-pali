e86a90eeb174a45ec10ca1002f1dca44df01b59c
tsubasamorisan <tsubasamori.eros@gmail.com>
2012-07-25 13:22:16 +0800

    update program for building index of pali words

--- tools/index.py
+++ tools/index.py
@@ -4,6 +4,7 @@
 # build index of pali words into JavaScript Array variables
 
 import os
+import xml.dom.minidom
 
 prefix_code = {
 #  "°" : "uc",
@@ -43,84 +44,44 @@ prefix_code = {
 }
 
 
-def handleIndexXML():
-  import xml.dom.minidom
-  dom = xml.dom.minidom.parse("paliIndex.xml")
-  items = dom.getElementsByTagName("word")
-
-  #print(items[0].toxml())
-  words = items[0].childNodes[0].data.encode('utf8').split('%')
-  print(words[0].decode('utf8'))
-  print(words[-1].decode('utf8'))
-
-  print(type(words))
-
-  prefix = {}
-
-  num = 0
-  for word in words:
-    y = unicode(word.decode('utf8'))
-    if y[:2] not in prefix:
-      prefix[y[:2]] = True
-    #print(y)
-    num += 1
-
-  print(num)
-
-  for key in prefix.keys():
-    print(key)
-
-  #x = unicode(words[2].decode('utf8'))
-  #print(len(x))
-  #print(x)
-  #print(x[3])
-
-  """
-  jscode = u"$(function(){var availableTags = ["
-  for word in words:
-    jscode += u'"%s",' % word.decode('utf8')
-
-  jscode += u"];"
-  jscode += u'$( "#tags" ).autocomplete({'
-  jscode += u'source: availableTags'
-  jscode += u'});'
-  jscode += u'});'
-
-  #print(jscode)
-
-  f = open('jscode.js', 'w')
-  f.write(jscode.encode('utf8'))
-  f.close()
-  """
-
-
 if __name__ == '__main__':
   xmldir = '/home/siongui/Desktop/pali-dict-software-web1version/xml/'
   fd = open("jsvarindex.js","w")
 
+  wordCount = 0
+  trueWordCount = 0
+
+  # get words start with prefix 'key'
   for key in prefix_code.keys():
-    #print(key)
-    #prefix = 'e'
     files = os.listdir(xmldir + key)
-    #print(files)
-    new_list = []
+    trueWordList = []
+
+    # iterate all words start with prefix 'key'
     for file in files:
-      x = unicode(file.decode('utf8'))
-      new_list.append(x)
+      wordCount += 1
+      fileName = unicode(file.decode('utf8'))
+      filaPath = xmldir + key + '/' + fileName.encode('utf-8')
+      fileData = open(filaPath.decode('utf-8'), 'r').read()
+      dom = xml.dom.minidom.parseString(fileData)
+      words = dom.getElementsByTagName('word')
+      isSameAsContent = False
+      for word in words:
+        wordStr = word.childNodes[0].data
+        if (wordStr.lower() == fileName[0:-4]):
+          trueWordList.append(fileName)
+          isSameAsContent = True
+          trueWordCount += 1
+          break
 
     jsArrayInt = u"var prefix_%s=[" % prefix_code[key]
 
-    num = 0
-    for x in new_list:
-      #print(x)
+    for x in trueWordList:
       jsArrayInt += u'"%s",' % x[:-4]
-      num += 1
 
     jsArrayInt = jsArrayInt[:-1]
     jsArrayInt += u"];\n"
-    #jsArrayInt += u"// number of words with prefix %s: %s\n" % (key.decode('utf8'), num)
-    #print(jsArrayInt)
     fd.write(jsArrayInt.encode('utf8'))
 
   fd.close()
-
+  print('word count: %s' % wordCount)
+  print('true word count: %s' % trueWordCount)
